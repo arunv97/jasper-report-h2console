77a430c09177d09fabb9c9a5466a0731a39f0296
Arun V Nair <arun.vinunair@juliusbaer.com>
2024-03-08 16:26:58 +0800

    Working for looping

--- src/main/java/com/arun/pdfreport/controller/ReportController.java
+++ src/main/java/com/arun/pdfreport/controller/ReportController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -68,7 +67,7 @@ public class ReportController {
         Portfolio portfolio1 = new Portfolio();
         portfolio1.setPortfolioNumber("SG091212-01");
         Map<String, Fee> fees1 = new HashMap<>();
-        fees1.put("BANKING_SERVICE_FEE", new Fee("Banking Service Package", "3000.00", "3000.00"));
+        fees1.put("BANKING_SERVICE_FEE", new Fee("Banking Service Package", "1299.00", "1222.00"));
         fees1.put("REPORTING_FEE", new Fee("Reporting Fee", "240.00", "240.00"));
         fees1.put("SPECIAL_MAILING", new Fee("Special Mailing", "700.00", "700.00"));
         portfolio1.setBankingFees(fees1);
@@ -77,11 +76,20 @@ public class ReportController {
         Portfolio portfolio2 = new Portfolio();
         portfolio2.setPortfolioNumber("SG091212-022");
         Map<String, Fee> fees2 = new HashMap<>();
-        fees2.put("BANKING_SERVICE_FEE", new Fee("Banking Service Package", "3000.00", "3000.00"));
+        fees2.put("BANKING_SERVICE_FEE", new Fee("Banking Service Package", "1200.00", "1000.00"));
         fees2.put("REPORTING_FEE", new Fee("Reporting Fee", "240.00", "240.00"));
         fees2.put("SPECIAL_MAILING", new Fee("Special Mailing", "700.00", "700.00"));
         portfolio2.setBankingFees(fees2);
         portfolios.add(portfolio2);
+
+        Portfolio portfolio3 = new Portfolio();
+        portfolio3.setPortfolioNumber("SG091212-03");
+        Map<String, Fee> fees3 = new HashMap<>();
+        fees3.put("BANKING_SERVICE_FEE", new Fee("Banking Service Package", "200.00", "20.00"));
+        fees3.put("REPORTING_FEE", new Fee("Reporting Fee", "240.00", "240.00"));
+        fees3.put("SPECIAL_MAILING", new Fee("Special Mailing", "700.00", "700.00"));
+        portfolio3.setBankingFees(fees3);
+        portfolios.add(portfolio3);
 
         PortfolioData portfolioData = new PortfolioData();
         portfolioData.setPortfolios(portfolios);
@@ -93,3 +101,17 @@ public class ReportController {
         return new JRBeanCollectionDataSource(accountDto);
     }
 }
+
+// PortfolioData
+// |
+// |-- List<Portfolio>
+//     |
+//     |-- Portfolio
+//         |
+//         |-- String portfolioNumber
+//         |
+//         |-- Map<String, Fee> bankingFees
+//             |
+//             |-- "BANKING_SERVICE_FEE" -> Fee(feeType, listPrice, effectivePrice)
+//             |-- "REPORTING_FEE" -> Fee(feeType, listPrice, effectivePrice)
+//             |-- "SPECIAL_MAILING" -> Fee(feeType, listPrice, effectivePrice)
